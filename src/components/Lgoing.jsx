ab2d393d4610613f28e886ea29c69dabcdbfb914
sadhin28 <taosifsadhin113@gmail.com>
2025-07-17 19:13:06 +0600

    login and registe implement

--- src/components/Lgoing.jsx
+++ src/components/Lgoing.jsx
@@ -1,12 +1,13 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Links, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
 import app from '../firebase/firebase.init';
+import { toast } from 'react-toastify';
 
 const Lgoing = () => {
     const navigate = useNavigate()
-    const { setUser, login } = useContext(AuthContext)
+    const { setUser, login,forgotPassword } = useContext(AuthContext)
 
 
 
@@ -19,11 +20,11 @@ const Lgoing = () => {
 
         login(email, password)
             .then(result => {
-                console.log(result.user)
+            
                 const lastSignInTime = result?.user?.metadata?.lastSignInTime
                 const loginInfo = { email, lastSignInTime }
                 //update user login time
-                fetch('', {
+                fetch('http://localhost:5000/user', {
                     method: "PATCH",
                     headers: {
                         'content-type': 'application/json'
@@ -32,9 +33,10 @@ const Lgoing = () => {
                 })
                     .then(res => res.json())
                     .then(data => {
-                        if (data.acknowledged == true) {
-                            navigate('/')
-                        }
+                     console.log(data)  
+                      if(data){
+                        navigate('/')
+                      }
 
                     })
 
@@ -56,6 +58,22 @@ const Lgoing = () => {
             .catch(error => {
                 navigate('/')
             })
+    }
+        //forgot password
+    const handelForgatePassword = () => {
+      const email =   document.getElementsByName('email')[0].value
+      if(!email){
+        toast.error("Please Provide A valid Email Address")
+      }
+      else{
+        forgotPassword(auth,email)
+        .then(res=>{
+            toast.success('Forgot Password,Please Check email')
+        })
+        .catch(error=>{
+            toast.error(error.message)
+        })
+      }
     }
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -104,12 +122,15 @@ const Lgoing = () => {
                     >
                         Login now
                     </button>
+                    <div className='mt-2 hover:underline'><Link onClick={handelForgatePassword}>Forgot password?</Link></div>
                     <div className='text-center mt-2'><span>You have no Acount? </span> <Link to='/register'><span className="text-green-600 hover:underline">Register Now</span></Link></div>
-                    <div className="text-center mt-2 mb-2">--------------- or ---------------</div>
+                     <div className="text-center mt-2 mb-2">--------------- or ---------------</div>
                     <button onClick={handelLoginWithGoogle} className="btn w-full bg-white flex justify-center items-center gap-2   text-black border p-2 rounded-xl ">
                         <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                         <div>Login with Google</div>
                     </button>
+                   
+                    
                 </form>
             </div>
         </div>
