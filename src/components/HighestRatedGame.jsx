a4e4796077f5598ae4d8b1f014ed893379482511
sadhin28 <taosifsadhin113@gmail.com>
2025-07-20 20:51:31 +0600

    add highest reated game

---
+++ src/components/HighestRatedGame.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+
+import { Link } from "react-router-dom";
+import { HomeContext } from "../layout/Home";
+import ReactStars from "react-stars";
+
+const HighestRatedGame = () => {
+
+    const topReviewsData = useContext(HomeContext);
+
+    return (
+        <div className="mt-5">
+            <h2 className="lg:text-5xl md:text-3xl text-xl font-medium text-center">Highest Rated Games:</h2>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5 mt-5">
+                {
+                    topReviewsData.map(review => (
+                        <div key={review._id} className="rounded-tr-none rounded-bl-none rounded-tl-3xl rounded-br-3xl shadow-lg bg-[url('/assets/gaming1.jpg')] bg-cover bg-center shadow-red-400 p-2 flex flex-col">
+                            <img src={review.game_image} alt="game cover image" className="rounded-tr-none rounded-bl-none rounded-tl-3xl rounded-br-3xl w-full aspect-[5/3] object-cover" />
+                            <div className="mt-2 text-black flex flex-col bg-[rgba(255,255,255,0.3)] flex-grow rounded-lg p-1">
+                                <h2 className="text-sm font-bold">Game Title: {review.game_title}</h2>
+                                <ReactStars count={5} value={review.rating} size={15} edit={false} activeColor="#ffd700" />
+                                <h2 className="text-sm flex-grow">
+                                    <span className="font-semibold">Description:</span> {review.review.split(' ').slice(0, 7).join(' ')}...
+                                </h2>
+                                <Link to={`/ReviewDetails/${review._id}`} className="border w-30 text-center rounded-xl hover:bg-green-200 mt-2  btn-neutral">Explore Details</Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default HighestRatedGame;
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import  { createContext, useEffect, useState } from 'react';
 import Slider from '../components/slider/Slider';
 import TopGame from '../components/TopGame';
+import { useLoaderData } from 'react-router-dom';
+import LoadingSpiner from '../components/LoadingSpiner'
+import HighestRatedGame from '../components/HighestRatedGame';
+
+export const HomeContext = createContext();
 
 const Home = () => {
+     const [loading, setLoading] = useState(true);
+
+    const shortedReviews = useLoaderData();
+
+    useEffect(() => {
+        if (shortedReviews) {
+            setLoading(false);
+        }
+    }, [shortedReviews]);
+
+    if (loading) {
+        return <LoadingSpiner></LoadingSpiner>
+    };
     return (
-        <div>
+        <div className='w-11/12 mx-auto'>
           <Slider></Slider>
-          <TopGame></TopGame>
+            <HomeContext.Provider value={shortedReviews}>
+               <HighestRatedGame></HighestRatedGame>
+            </HomeContext.Provider>
+             <TopGame></TopGame>
         </div>
     );
 };
